dc83786e17bac206f7fe9ecc848d7071d0f3bd91
aldiggy <aldikh@gmail.com>
2022-04-13 12:03:03 +0700

    update dark mode + perbaikan fitur

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   content: ["index.html"],
+  darkMode: "class",
   theme: {
     container: {
       center: true,
@@ -8,6 +9,9 @@ module.exports = {
     extend: {
       colors: {
         primary: "#FFC451",
+        home: "#06113c",
+        portfolio: "#ff8c32",
+        blog: "#dddddd",
         rred: "#FF033E",
         secondary: "#64748b",
         dark: "#0f172a",
